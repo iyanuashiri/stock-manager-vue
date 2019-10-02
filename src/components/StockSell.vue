dbd79a7aa5d4147679361451a9bda7bb568f1aba
iyanuashiri <ajaoiyanu@gmail.com>
2019-10-02 03:19:36 +0100

    Add StockSell

---
+++ src/components/StockSell.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="StockSell">
+        <NavBar></NavBar>
+
+        <form v-on:submit.prevent="sellStock">
+            <div>
+                <label for="symbol"></label>
+                <input id="symbol" name="symbol" type="text" v-model="model.symbol">
+            </div>
+            <div>
+                <label for="price"></label>
+                <input id="price" name="price" type="text" v-model="model.shares">
+            </div>
+            <div>
+                <button type="submit">Buy</button>
+            </div>
+        </form>
+    </div>
+
+</template>
+
+<script>
+    import api from "../api";
+    import NavBar from "./NavBar";
+    export default {
+        name: "StockSell",
+        components: {NavBar},
+        data () {
+            return {
+                model: {}
+            }
+        },
+        methods: {
+            sellStock: async function () {
+                await api.sellStock(this.model)
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
